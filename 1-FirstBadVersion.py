219c8f597809b85d7ce4f107f06b184456eb0866
JonathanSumon <jonathansumon@gmail.com>
2020-05-02 01:52:31 +0530

    1st May Challenge Solution

---
+++ 1-FirstBadVersion.py
@@ -0,0 +1,52 @@
+"""
+You are a product manager and currently leading a team to develop a new product. 
+
+Unfortunately, the latest version of your product fails the quality check. 
+
+Since each version is developed based on the previous version, all the versions after a 
+
+bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] 
+
+and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. 
+
+Implement a function to find the first bad version. 
+
+You should minimize the number of calls to the API.
+
+"""
+
+
+
+#Solution
+
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        #Binary Search
+        
+        start = 1
+        end = n
+        
+        if n == 1: return 1
+        
+        while start < end:
+            middle = start + (end-start)//2
+            if isBadVersion(middle) :
+                end = middle
+                
+            else:
+                start = middle + 1
+                
+        return int(start)
+        
+        
