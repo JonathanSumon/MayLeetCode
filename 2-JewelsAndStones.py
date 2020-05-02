bc6aa7dd3033bc8505a80cde5682887b88d1e43c
JonathanSumon <jonathansumon@gmail.com>
2020-05-02 16:32:25 +0530

    Removing Colcon Build

--- 2-JewelsAndStones.py
+++ 2-JewelsAndStones.py
@@ -16,7 +16,7 @@ class Solution:
         for i in range(len(J)):
             for j in range(len(S)):
                 if J[i]==S[j]:
-                        count+=1
+                    count+=1
                         
         return int(count)
                 
